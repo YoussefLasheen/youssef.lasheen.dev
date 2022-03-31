5c8f94fc5ded04830feef2952fb012301162a78d
Youssef Lasheen <youssef@lasheen.dev>
2022-03-30 18:38:57 -0700

    Fix scrollbar not sticking to the side bug

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -23,58 +23,60 @@ class _MainScreenState extends State<MainScreen> {
         child: SingleChildScrollView(
           primary: true,
           physics: BouncingScrollPhysics(),
-          child: ConstrainedBox(
-            constraints: BoxConstraints.expand(width: 600, height: 1000),
-            child: Column(
-              children: [
-                ProfileWidget(
-                  imagePath: 'https://avatars.githubusercontent.com/u/28854622?v=4',
-                ),
-                const SizedBox(height: 24),
-                buildName(),
-                const SizedBox(height: 15),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 50),
-                  child: SizedBox(
-                    height: 80,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      mainAxisSize: MainAxisSize.max,
-                      children: const <Widget>[
-                        ProfileInfoCard(
-                          text: "INTP",
-                          iconData: FontAwesomeIcons.iCursor,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        ProfileInfoCard(
-                          text: "Developer",
-                          iconData: Icons.developer_mode_rounded,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        ProfileInfoCard(
-                          text: "Engineering\nStudent",
-                          iconData: Icons.engineering_rounded,
-                        ),
-                      ],
+          child: Center(
+            child: ConstrainedBox(
+              constraints: BoxConstraints.expand(width: 600, height: 1000),
+              child: Column(
+                children: [
+                  ProfileWidget(
+                    imagePath: 'https://avatars.githubusercontent.com/u/28854622?v=4',
+                  ),
+                  const SizedBox(height: 24),
+                  buildName(),
+                  const SizedBox(height: 15),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 50),
+                    child: SizedBox(
+                      height: 80,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        mainAxisSize: MainAxisSize.max,
+                        children: const <Widget>[
+                          ProfileInfoCard(
+                            text: "INTP",
+                            iconData: FontAwesomeIcons.iCursor,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          ProfileInfoCard(
+                            text: "Developer",
+                            iconData: Icons.developer_mode_rounded,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          ProfileInfoCard(
+                            text: "Engineering\nStudent",
+                            iconData: Icons.engineering_rounded,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 35),
-                Wrap(
-                  runSpacing: 10,
-                  children: [
-                    SpotifyBigCard(),
-                    StackoverflowBigCard(),
-                    DiscordBigCard(),
-                    GithubBigCard(),
-                    CodeforcesBigCard()
-                  ],
-                ),
-              ],
+                  const SizedBox(height: 35),
+                  Wrap(
+                    runSpacing: 10,
+                    children: [
+                      SpotifyBigCard(),
+                      StackoverflowBigCard(),
+                      DiscordBigCard(),
+                      GithubBigCard(),
+                      CodeforcesBigCard()
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),

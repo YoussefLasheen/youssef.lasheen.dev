fc14f7976b5d838d7677afddad638eda211c8b04
Youssef Lasheen <youssef@lasheen.dev>
2022-04-01 01:03:34 +0200

    Update the website's title, description, and favicon

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Youssef\'s Links',
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.dark,
       home: MainScreen(),

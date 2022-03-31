a4ed87063208fea60b35a2c4a758326d8909c560
Youssef Lasheen <youssef@lasheen.dev>
2022-03-31 23:20:44 +0200

    Add on hover animation to small info cards with foil

--- lib/components/small_info_cards/shared_components/profile_info_card.dart
+++ lib/components/small_info_cards/shared_components/profile_info_card.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:foil/foil.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class ProfileInfoCard extends StatelessWidget {
+class ProfileInfoCard extends StatefulWidget {
   final String text;
   final IconData iconData;
 
@@ -10,21 +11,44 @@ class ProfileInfoCard extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<ProfileInfoCard> createState() => _ProfileInfoCardState();
+}
+
+class _ProfileInfoCardState extends State<ProfileInfoCard> {
+  bool hovering = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: AspectRatio(
-        aspectRatio: 1,
-        child: Card(
-          elevation: 12,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              FaIcon(
-                iconData
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      onEnter: (_) {
+        setState(() {
+          hovering = true;
+        });
+      },
+      onExit: (_) {
+        setState(() {
+          hovering = false;
+        });
+      },
+      child: Center(
+        child: AspectRatio(
+          aspectRatio: 1,
+          child: Card(
+            elevation: 12,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+            child: Foil(
+              isUnwrapped: !hovering,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FaIcon(
+                    widget.iconData
+                  ),
+                  Text(widget.text, textAlign: TextAlign.center, style: TextStyle(fontSize: 12),)
+                ],
               ),
-              Text(text, textAlign: TextAlign.center, style: TextStyle(fontSize: 12),)
-            ],
+            ),
           ),
         ),
       ),
